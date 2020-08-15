00fe2e1c5e5cd2dd056ce24598a94951b9e88b82
alissone-neyret <aly3@live.fr>
2020-08-15 22:39:11 +0200

    feature: mise en place de l'animation de retournement et de ses implications, feature: affichage d'une modale quand on gagne, feature: mise en place de la progress bar

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,16 @@
 $(document).ready(function () {
 
     // On définit notre tableau de fruit
-    const listeFruits = [
-        {
-            reference: 1,
-        },
-        {
-            reference: 2,
-        },
-        {
-            reference: 3,
-        },
-        {
-            reference: 4,
-        },
-
-    ]
-    const tableauCartes = []
+    const listeFruits = [{ reference: 1 }, { reference: 2 }, { reference: 3 }, { reference: 4, }, { reference: 5 }, { reference: 6 }, { reference: 7 }, { reference: 8 }, { reference: 9 }, { reference: 10 }, { reference: 11 }, { reference: 12 }, { reference: 13 }, { eference: 14 }, { reference: 15 }, { reference: 16 }, { eference: 17 }, { reference: 18 }];
+    const tableauCartes = [];
+    let cartesRetournees = [];
+    const pairesTrouvees = [];
 
     // On crée un classe "Partie" qui va servir à contenir toutes les actions voulues qui concernent la partie de jeu
     class Partie {
         constructor() {
             this.nouvellePartie()
+            this.barreDeProgression()
         }
 
         // On démarre une partie
@@ -50,26 +39,79 @@ $(document).ready(function () {
             })
 
             this.melangeDesCartes(tableauCartes)
-            
-            
+
+
             $.each(tableauCartes, function (index, carte) {
 
                 console.log("carte", carte)
                 console.log("index", index)
-                $('#plateau_cartes').append(`<div id=${index} class="carte"><div class="carte-face-arriere"></div>
+                $('#memory__plateau__cartes').append(`<div id=${index} class="carte"><div class="carte-face-arriere"></div>
                 <div class="carte-face-avant" style="background-position-y: ${carte.positionBackground}px"></div></div>`)
-                
+
             })
 
 
+            $(".carte").click(async (e) => {
+                let carteCliquee = tableauCartes[e.currentTarget.id]
 
-            $(".carte").click(function (e) {
-                console.log("click", e.currentTarget.id)
-                $(`#${e.currentTarget.id}`).addClass('--animation-retournement-carte')
+                console.log("carte cliquée", carteCliquee)
+                console.log("carte paires trouvees", pairesTrouvees.indexOf(carteCliquee.id))
+                if (pairesTrouvees.length > 0 && pairesTrouvees.indexOf(carteCliquee.id) !== -1) {                    
+                        return false                     
+                } else if (cartesRetournees.length === 0) {
+                    $(`#${e.currentTarget.id}`).addClass('--animation-retournement-carte')
+                    carteCliquee.idElement = parseInt(e.currentTarget.id)
+                    cartesRetournees.push(carteCliquee)
+
+                }
+                else if (cartesRetournees.length === 1) {
+                    $(`#${e.currentTarget.id}`).addClass('--animation-retournement-carte')
+                    carteCliquee.idElement = parseInt(e.currentTarget.id)
+                    cartesRetournees.push(carteCliquee)
+
+                    if (cartesRetournees[0].id === cartesRetournees[1].id) {
+                        console.log("une paire de trouvée !")
+                        pairesTrouvees.push(cartesRetournees[0].id)
+                        pairesTrouvees.push(cartesRetournees[1].id)
+                        cartesRetournees = []
+
+                        if (tableauCartes.length === 0) {
+                            console.log("gagné!!")
+                            window.setTimeout(() => {
+                                alert("C'est gagné")
+                            }, 700)
+                        }
+
+                    } else {
+
+                        console.log("cartes retournées tableau", cartesRetournees)
+                        window.setTimeout(() => {
+                            console.log("cartes retournées tableau", cartesRetournees)
+
+                            console.log("je time out")
+                            $(`#${cartesRetournees[0].idElement}`).removeClass('--animation-retournement-carte')
+                            $(`#${cartesRetournees[1].idElement}`).removeClass('--animation-retournement-carte')
+                            cartesRetournees = []
+
+
+                        }, 700)
+
+                    }
+
+                }
+                else if (cartesRetournees.length > 1) {
+                    return false
+                }
+
             })
 
         }
 
+
+        /**
+         * Permet de modifier les emplacements des cartes de façon aléatoire
+         * @param {array} tableauCartes 
+         */
         melangeDesCartes(tableauCartes) {
             for (let i = tableauCartes.length - 1; i > 0; i--) {
 
@@ -80,6 +122,39 @@ $(document).ready(function () {
             }
         }
 
+        compareCartesRetournees(carte) {
+
+            cartesRetournees.push(carte)
+            console.log("carte retournées tableau", cartesRetournees)
+            if (cartesRetournees.length === 2) {
+                console.log("carte retournée 1", cartesRetournees[0].id)
+                console.log("carte retournée 2", cartesRetournees[1].id)
+                let unePaireTrouvee = cartesRetournees[0].id === cartesRetournees[1].id
+                if (unePaireTrouvee) {
+                    console.log("une paire de trouvée !")
+                    pairesTrouvees.push(cartesRetournees[0].id)
+                }
+            }
+        }
+
+        barreDeProgression() {
+
+            console.log("progression")
+            let elem = document.getElementById("chrono");
+            let width = 1;
+            let id = setInterval(frame, 1000);
+
+            console.log("elem", elem)
+            function frame() {
+                if (width >= 100) {
+                    clearInterval(id);
+                } else {
+                    width++;
+                    elem.style.width = width + '%';
+                }
+            }
+
+        }
 
 
     }
