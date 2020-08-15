5dcc9fb390b9ffca02803354e06cec3b645d3e9d
alissone-neyret <aly3@live.fr>
2020-08-15 12:39:10 +0200

    feature: mise en place du squelette

---
+++ public/js/carte.js
@@ -0,0 +1,6 @@
+export default class Carte {
+    constructor(nom, id) {
+        this.id = id;
+        this.nom = nom;
+    }
+}
---
+++ public/js/partie.js
@@ -0,0 +1,32 @@
+const cartes = [
+    {
+        id: 1,
+        nom: "Pomme",
+    },
+    {
+        id: 1,
+        nom: "Pomme",
+    },
+    {
+        id: 2,
+        nom: "Banane",
+    },
+    {
+        id: 2,
+        nom: "Banane",
+    },
+
+]
+
+export class Partie {
+    constructor() {
+        this.nouvellePartie()
+    }
+
+    nouvellePartie() {
+        $.each(cartes, function (index, value) {
+            $('.plateau-cartes').append(index + ":" + value + '<br>')
+        })
+    }
+
+}
