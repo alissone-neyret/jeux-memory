f2356de68b8a864fca8e5a193100a433340a3bc8
alissone-neyret <aly3@live.fr>
2020-08-17 14:16:21 +0200

    fix: affichage du fichier config.js pour permettre d'accéder à la config de la base de données

---
+++ config.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+
+    host: "localhost",
+
+    user: "root",
+
+    password: "password",
+
+    database: "memory"
+});
+
+module.exports = connection;
+
+connection.connect(function (err) {
+
+    if (err) throw err;
+
+    console.log("Connecté à la base de données MySQL!");
+    connection.query("CREATE DATABASE IF NOT EXISTS memory", function (err, result) {
+      if (err) throw err;
+    });
+
+    var sql = "CREATE TABLE IF NOT EXISTS score (id INT AUTO_INCREMENT PRIMARY KEY, duree VARCHAR(255), aGagne TINYINT(1))";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+    });
+
+});
